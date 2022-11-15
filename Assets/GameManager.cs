1f938749aa561e1b800329c26097b204400359d6
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2022-11-15 17:40:21 +0900

    簡易的なゲームオーバー画面を作成しました

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreTest.score >= 100) {
+            //ゲームクリア
+            Debug.Log("ゲームクリア");
+        } else {
+            Invoke(nameof(GameOver), 30);
+        }
+        
+    }
+
+    public void GameOver() {
+        //ゲームオーバー
+            Debug.Log("ゲームオーバー");
+            gameOverUI.SetActive(true);
+    }
+
+    public void GameReplay() {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
